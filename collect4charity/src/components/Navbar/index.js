e62f61ea5a72ffbf44b45db37a5f781e53e49c04
Onyeoziri I <Onyeoziri@gmail.com>
2022-04-12 20:34:25 -0400

    added pages and Navbar

---
+++ collect4charity/src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Nav, 
+  NavLink, 
+  Bars, 
+  NavMenu, 
+  NavBtn, 
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <NavLink to="">
+          <h1>Logo</h1>
+        </NavLink>
+        // <Bars/>  //hamburger menu
+
+        <NavMenu>
+          <NavLink to="/home" activeStyle>
+            Home
+          </NavLink>
+
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+
+          <NavLink to="/charities" activeStyle>
+            Charities
+          </NavLink>
+        </NavMenu>
+
+        <NavBtn>
+          <NavBtnLink to="/login"> Log In</NavBtnLink>
+        </NavBtn>
+
+    </Nav>
+  )
+}
+
+export default Navbar;
