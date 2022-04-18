f89d92d2e42ef6ca90c62b5bbc854c5fb8e0d3f2
Onyeoziri I <Onyeoziri@gmail.com>
2022-04-18 14:32:20 -0400

    file structure update

--- collect4charity/src/App.js
+++ collect4charity/src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <div className = 'semi-circle'></div>
       <Router>
         <Navbar/>
       </Router> 
