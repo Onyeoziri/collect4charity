efc5a220d7347ab221560b3fd3094c24841406e7
iiFahad <104042771+iiFahad@users.noreply.github.com>
2022-05-09 22:39:35 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-export {default as Home} from './Home/home';
-export {default as Charities} from './Charities/charities';
-export {default as About} from './About/about';
-export {default as Login} from './login/loginForm';
-export {default as Services} from './Services/services';
+export {default as Home} from './Home/Home';
+export {default as Charities} from './Charities/Charities';
+export {default as About} from './About/About';
+export {default as Login} from './login/LoginForm';
+export {default as Services} from './Services/Services';
 export {default as ErrorPage} from './ErrorPage/ErrorPage'; 
