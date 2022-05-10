c0aa820ac9feb32400ffac691946d912ea1d6aac
JakeD6 <jid12158@gmail.com>
2022-05-10 13:30:34 -0400

    Added Functional Express API, Changed Database Connection to pool, npm run express will run express api

--- src/pages/Charities/Charities.js
+++ src/pages/Charities/Charities.js
@@ -1,9 +1,78 @@
-import React, {useState, useEffect} from 'react'
+import { data } from 'cheerio/lib/api/attributes'
+import React, {useState, useEffect, Component} from 'react'
 import DataTable from '../Services/DataTable'
+import axios from 'axios'
+import './charities.css'
+
+/*
+function Charities(){
+  const [data, setData] = useState({})
+
+  useEffect(() =>{
+    fetch("http://localhost:3001/charities")
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, [])
+  return(
+    <div>{ <DataTable data={data}/> }</div>
+  )
+}
+*/
 
 
+class Charities extends Component{
+  state={
+    charities: []
+  }
+  
+  
+  componentDidMount(){
+    axios.get('http://localhost:4000/charities')
+    .then(res => {
+      console.log(res);
+      const charities = res.data
+      this.setState({charities})
+    })
+  }
+  
+  
 
-function Charities() {
+  /*
+callBackendAPI() {
+  fetch("http://localhost:3001/charities")
+  .then(res => res.text())
+  .then(res => this.setState({ apiResponse: res}))
+  .catch(err => err);
+}
+*/
+render(){
+  return(
+    <div>
+      <p>
+        <table>
+          <th>Name</th>
+          <th>Location</th>
+          <th>ID</th>
+          <th>Category</th>
+       {this.state.charities.map(charity =>
+       <tr>
+        <td>{charity.charityName} </td>
+        <td>{charity.location}</td>
+        <td>{charity.charityID}</td>
+        <td>{charity.category}</td>
+        </tr>
+        )}
+        </table>
+      </p>
+      <p>
+
+      </p>
+    </div>
+
+  )
+}
+ 
+
 /*   const[data, setData] = useState([])
   const [q, setQ] = useState
 
@@ -13,11 +82,12 @@ function Charities() {
     .then((json) => setData(json))
   }, []) */
 
-  return (
-    <div>
-      {/* <DataTable data={data}/> */}
-    </div>
-  )
-}
+  // return (
+  //   <div>
+  //     {/* <DataTable data={data}/> */}
+  //   </div>
+  // )
 
+//}
+}
 export default Charities
--- src/server/dbConnect.js
+++ src/server/dbConnect.js
@@ -10,13 +10,28 @@ const fs = require('fs');
 let important = fs.readFileSync('./src/important.json');
 let importantData = JSON.parse(important);
 
+var connectionInfo = {
+    host: importantData.host,
+    port: importantData.port,
+    database: importantData.database,
+    user: importantData.user,
+    password: importantData.password,
+    connectionLimit: 10
+}
+
+var dbconnection = mysql.createPool(connectionInfo);
+
+/*
 const dbconnection = mysql.createConnection({
     host: importantData.host,
     port: importantData.port,
     database: importantData.database,
     user: importantData.user,
     password: importantData.password,
+    connectTimeout: 50000
 });
+
+/
 
 dbconnection.connect((err) => {
     if(err){
@@ -25,6 +40,9 @@ dbconnection.connect((err) => {
         console.log("Connection created with MySQL database successfully!");
     }
 });
+
+*/
+
 
 app.post('login', (req, res) => {
     const username = req.body.username;
